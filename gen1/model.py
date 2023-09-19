3e1dc9197a14b18c09a6e4c17d5144161365e8f0
Kye <kye@apacmediasolutions.com>
2023-09-18 21:11:51 -0400

    forward

--- gen1/model.py
+++ gen1/model.py
@@ -1,23 +1,55 @@
 import torch 
-from einops
+import einops
+from einops import rearrange, reduce, repeat
 from torch import nn
 from shapeless import liquid
 
 @liquid
 class AutoEncoder(nn.Module):
     dim: int
+    x: torch.Tensor
+    out_channels: int = 128
+    stide: int
+    padding: int
+    groups: int  = 32
+    eps = 1e-5
+    in_channels = 3
 
     def forward(self, x):
-        x = nn.Flatten(x, self.dim)
-        x = nn.Linear(x, 128)
-        x = nn.ReLU(x)
-        x = nn.Linear(x, 64)
-        x = nn.ReLU(x)
-        x = nn.Linear(x, 128)
-        x = nn.ReLU(x)
-        x = nn.Linear(x, 784)
-        x = nn.Reshape(x, self.dim)
-        return x
+        x = nn.Sequential(
+            nn.Conv2d(
+                in_channels=self.in_channels, 
+                out_channels=self.out_channels,
+                kernel_size=3,
+                stride=self.stide,
+                padding=self.padding,
+            ),
+            nn.GroupNorm(num_groups=self.groups, eps=self.eps),
+            nn.ELU(),
+
+            nn.Conv2d(
+                #3x3 depthwise conv, 128 channels, 3 kernel, 1 pad
+                in_channels=self.in_channels,
+                out_channels=self.out_channels,
+                kernel_size=3,
+                stride=self.stride,
+                padding=1
+            ),
+            nn.GroupNorm(self.groups, self.eps),
+            nn.ELU(),
+
+            nn.Conv3d(
+                in_channels=self.in_channels,
+                out_channels=256,
+                kernel_size=3,
+                stride=2,
+                padding=1
+            ),
+            nn.GroupNorm(self.groups, self.eps),
+            nn.ELU()
+
+
+        )
     
 
 @liquid
