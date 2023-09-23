863c84dec3f8116fa891b8db1e064780c6992ff6
Kye <kye@apacmediasolutions.com>
2023-09-23 19:54:11 -0400

    pipeline, image, text, => midas => cleanup

--- gen1/pipeline.py
+++ gen1/pipeline.py
@@ -5,17 +5,13 @@ from gen1.clip import CLIP
 class Pipeline(nn.Module):
     def __init__(
         self,
-        image,
-        video
     ):
-        self.image = image
-        self.video = video
 
         self.midas = MidasHF()
         self.clip = CLIP()
         
-    def forward(self):
-        image = self.midas.forward(self.image)
+    def forward(self, image):
+        image = self.midas.forward(image)
         image_embeddings = self.clip.run(image)
         return image_embeddings
     
