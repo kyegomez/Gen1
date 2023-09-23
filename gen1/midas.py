59c3278748adf4127c44cef36faf7b5fd4fb19b1
Kye <kye@apacmediasolutions.com>
2023-09-23 13:53:40 -0400

    midas option2

--- gen1/midas.py
+++ gen1/midas.py
@@ -5,6 +5,9 @@ https://github.com/thomasjpfan/pytorch_refinenet/blob/master/pytorch_refinenet/r
 import torch
 import torch.nn as nn
 
+#option 2
+
+
 #utils
 
 
@@ -507,3 +510,50 @@ class Midas(BaseModel):
         out = self.scratch.output_conv(path_1)
 
         return torch.squeeze(out, dim=1)
+    
+
+#or option2
+from PIL import Image
+import numpy as np
+import requests
+import torch
+from transformers import DPTForDepthPrediction, DPTConfig, DPTFeatureExtractor
+
+
+class MidasHF:
+    def __init__(
+        self,
+        model_name_or_path: str = 'Intel/dpt-hybrid-midas',
+        image: str = None
+    ):
+        self.model_name_or_path = model_name_or_path
+        self.model = DPTForDepthPrediction.from_pretrained(
+            'Intel/dpt-hybrid-midas',
+            low_cpu_mem_usage=True
+        )
+        self.feature_extractor = DPTFeatureExtractor.from_pretrained(
+            self.model_name_or_path
+        )
+
+        self.image = Image.open(requests.get(image, stream=True).raw)
+    
+    def forward(self):
+        inputs = self.feature_extractor(
+            images=self.images,
+            return_tensors="pt"
+        )
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            predicted_depth = outputs.predicted_depth
+
+            #interpolate to original size
+            prediction = torch.nn.functional.interpolate(
+                predicted_depth.unsqueeze(1),
+                size=self.image.size[::-1],
+                mode="bicubic",
+                align_corners=False
+            )
+        
+
+
