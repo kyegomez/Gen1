864ddbc565a4fa9192e16265dabfe9078dfc336c
Kye <kye@apacmediasolutions.com>
2023-09-23 20:44:06 -0400

    gen1

--- gen1/attend.py
+++ gen1/attend.py
@@ -1,37 +1,20 @@
 from collections import namedtuple
-from dataclasses import dataclass
-from functools import partial, wraps
-from typing import Optional
+from functools import wraps
 
 import torch
 import torch.nn.functional as F
-from einops import rearrange, repeat
+from einops import rearrange
 from packaging import version
-from torch import Tensor, einsum, nn
+from torch import einsum, nn
 
 # constants
 
-EfficientAttentionConfig = namedtuple('EfficientAttentionConfig', ['enable_flash', 'enable_math', 'enable_mem_efficient'])
-
-@dataclass
-class Intermediates:
-    qk_similarities: Optional[Tensor] = None
-    pre_softmax_attn: Optional[Tensor] = None
-    post_softmax_attn: Optional[Tensor] = None
-
-    def to_tuple(self):
-        return (self.qk_similarities, self.pre_softmax_attn, self.post_softmax_attn)
+AttentionConfig = namedtuple('AttentionConfig', ['enable_flash', 'enable_math', 'enable_mem_efficient'])
 
 # helpers
 
 def exists(val):
     return val is not None
-
-def default(val, d):
-    return val if exists(val) else d
-
-def compact(arr):
-    return [*filter(exists, arr)]
 
 def once(fn):
     called = False
@@ -46,74 +29,27 @@ def once(fn):
 
 print_once = once(print)
 
-# functions for creating causal mask
-# need a special one for onnx cpu (no support for .triu)
-
-def create_causal_mask(i, j, device):
-    return torch.ones((i, j), device = device, dtype = torch.bool).triu(j - i + 1)
-
-def onnx_create_causal_mask(i, j, device):
-    r = torch.arange(i, device = device)
-    causal_mask = rearrange(r, 'i -> i 1') < rearrange(r, 'j -> 1 j')
-    causal_mask = F.pad(causal_mask, (j - i, 0), value = False)
-    return causal_mask
-
 # main class
 
 class Attend(nn.Module):
     def __init__(
         self,
-        *,
         dropout = 0.,
-        causal = False,
-        heads = None,
-        talking_heads = False,
-        sparse_topk = None,
-        scale = None,
-        qk_norm = False,
         flash = False,
-        add_zero_kv = False,
-        onnxable = False
+        causal = False
     ):
         super().__init__()
-        self.scale = scale
-        self.qk_norm = qk_norm
-
-        self.causal = causal
-        self.create_causal_mask = onnx_create_causal_mask if onnxable else create_causal_mask
-
-        self.attn_fn = partial(F.softmax, dtype = torch.float32) if not qk_norm else F.softmax
-
         self.dropout = dropout
         self.attn_dropout = nn.Dropout(dropout)
 
-        # talking heads
-
-        assert not (flash and talking_heads), 'talking heads not compatible with flash attention'
-
-        self.talking_heads = talking_heads
-        if talking_heads:
-            self.pre_softmax_talking_heads = nn.Conv2d(heads, heads, 1, bias = False)
-            self.post_softmax_talking_heads = nn.Conv2d(heads, heads, 1, bias = False)
-
-        # sparse topk
-
-        assert not (flash and sparse_topk), 'sparse topk not compatible with flash attention'
-        self.sparse_topk = sparse_topk
-
-        # add a key / value token composed of zeros
-        # in case this helps controlling outliers, proposed by https://www.evanmiller.org/attention-is-off-by-one.html
-
-        self.add_zero_kv = add_zero_kv
-
-        # flash attention
+        self.causal = causal
 
         self.flash = flash
         assert not (flash and version.parse(torch.__version__) < version.parse('2.0.0')), 'in order to use flash attention, you must be using pytorch 2.0 or above'
 
         # determine efficient attention configs for cuda and cpu
 
-        self.cpu_config = EfficientAttentionConfig(True, True, True)
+        self.cpu_config = AttentionConfig(True, True, True)
         self.cuda_config = None
 
         if not torch.cuda.is_available() or not flash:
@@ -123,96 +59,32 @@ class Attend(nn.Module):
 
         if device_properties.major == 8 and device_properties.minor == 0:
             print_once('A100 GPU detected, using flash attention if input tensor is on cuda')
-            self.cuda_config = EfficientAttentionConfig(True, False, False)
+            self.cuda_config = AttentionConfig(True, False, False)
         else:
             print_once('Non-A100 GPU detected, using math or mem efficient attention if input tensor is on cuda')
-            self.cuda_config = EfficientAttentionConfig(False, True, True)
+            self.cuda_config = AttentionConfig(False, True, True)
 
-    def flash_attn(
-        self,
-        q, k, v,
-        mask = None,
-        attn_bias = None
-    ):
-        batch, heads, q_len, _, k_len, is_cuda, device = *q.shape, k.shape[-2], q.is_cuda, q.device
+    def flash_attn(self, q, k, v):
+        _, heads, q_len, _, k_len, is_cuda = *q.shape, k.shape[-2], q.is_cuda
 
-        # Recommended for multi-query single-key-value attention by Tri Dao
-        # kv shape torch.Size([1, 512, 64]) -> torch.Size([1, 8, 512, 64])
-
-        if k.ndim == 3:
-            k = rearrange(k, 'b ... -> b 1 ...').expand_as(q)
-
-        if v.ndim == 3:
-            v = rearrange(v, 'b ... -> b 1 ...').expand_as(q)
-
-        # handle scale - by default they scale by dim_head ** -0.5, but need to take care if using cosine sim attention
-
-        if self.qk_norm:
-            default_scale = q.shape[-1] ** -0.5
-            q = q * (default_scale / self.scale)
-
-        # Check if mask exists and expand to compatible shape
-        # The mask is B L, so it would have to be expanded to B H N L
-
-        causal = self.causal
-
-        if exists(mask):
-            assert mask.ndim == 4
-            mask = mask.expand(batch, heads, q_len, k_len)
-
-            # manually handle causal mask, if another mask was given
-
-            if causal:
-                causal_mask = self.create_causal_mask(q_len, k_len, device = device)
-                mask = mask & ~causal_mask
-                causal = False
-
-        # handle alibi positional bias
-        # convert from bool to float
-
-        if exists(attn_bias):
-            attn_bias = rearrange(attn_bias, 'h i j -> 1 h i j').expand(batch, heads, -1, -1)
-
-            # if mask given, the mask would already contain the causal mask from above logic
-            # otherwise, if no mask given but still causal, mask out alibi positional bias to a large negative number
-
-            mask_value = -torch.finfo(q.dtype).max
-
-            if exists(mask):
-                attn_bias = attn_bias.masked_fill(~mask, mask_value // 2)
-            elif causal:
-                causal_mask = self.create_causal_mask(q_len, k_len, device = device)
-                attn_bias = attn_bias.masked_fill(causal_mask, mask_value // 2)
-                causal = False
-
-            # scaled_dot_product_attention handles attn_mask either as bool or additive bias
-            # make it an additive bias here
-
-            mask = attn_bias
+        q, k, v = map(lambda t: t.contiguous(), (q, k, v))
 
         # Check if there is a compatible device for flash attention
 
         config = self.cuda_config if is_cuda else self.cpu_config
 
         # pytorch 2.0 flash attn: q, k, v, mask, dropout, causal, softmax_scale
-        
+
         with torch.backends.cuda.sdp_kernel(**config._asdict()):
             out = F.scaled_dot_product_attention(
                 q, k, v,
-                attn_mask = mask,
-                dropout_p = self.dropout if self.training else 0., 
-                is_causal = causal
+                dropout_p = self.dropout if self.training else 0.,
+                is_causal = self.causal
             )
 
-        return out, Intermediates()
+        return out
 
-    def forward(
-        self,
-        q, k, v,
-        mask = None,
-        attn_bias = None,
-        prev_attn = None
-    ):
+    def forward(self, q, k, v, bias = None):
         """
         einstein notation
         b - batch
@@ -221,82 +93,37 @@ class Attend(nn.Module):
         d - feature dimension
         """
 
-        n, heads, kv_heads, device = q.shape[-2], q.shape[1], k.shape[1], q.device
-
-        scale = default(self.scale, q.shape[-1] ** -0.5)
-
-        # handle grouped multi-query attention
-
-        if kv_heads == 1:
-            k, v = map(lambda t: rearrange(t, 'b 1 n d -> b n d'), (k, v))
-        elif kv_heads < heads:
-            k, v = map(lambda t: repeat(t, 'b kvh n d -> b (r kvh) n d', r = heads // kv_heads), (k, v))
-
-        # handle zero kv, as means for allowing network to attend to nothing
-
-        if self.add_zero_kv:
-            k, v = map(lambda t: F.pad(t, (0, 0, 1, 0), value = 0.), (k, v))
-
-            if exists(mask):
-                mask = F.pad(mask, (1, 0), value = True)
-
-            if exists(attn_bias):
-                attn_bias = F.pad(attn_bias, (1, 0), value = 0.)
+        q_len, k_len, device = q.shape[-2], k.shape[-2], q.device
 
         if self.flash:
-            assert not exists(prev_attn), 'residual attention not compatible with flash attention'
-            return self.flash_attn(q, k, v, mask = mask, attn_bias = attn_bias)
+            assert not exists(bias)
+            return self.flash_attn(q, k, v)
 
-        kv_einsum_eq = 'b j d' if k.ndim == 3 else 'b h j d'
+        scale = q.shape[-1] ** -0.5
 
-        dots = einsum(f'b h i d, {kv_einsum_eq} -> b h i j', q, k) * scale
+        # similarity
 
-        if exists(prev_attn):
-            dots = dots + prev_attn
+        sim = einsum(f"b h i d, b h j d -> b h i j", q, k) * scale
 
-        qk_similarities = dots.clone()
+        # attn bias
 
-        if self.talking_heads:
-            dots = self.pre_softmax_talking_heads(dots)
+        if exists(bias):
+            sim = sim + bias
 
-        if exists(attn_bias):
-            dots = dots + attn_bias
-
-        i, j, dtype = *dots.shape[-2:], dots.dtype
-
-        mask_value = -torch.finfo(dots.dtype).max
-
-        if exists(self.sparse_topk) and self.sparse_topk < j:
-            top_values, _ = dots.topk(self.sparse_topk, dim = -1)
-            sparse_topk_mask = dots < top_values[..., -1:]
-            mask = (mask & sparse_topk_mask) if exists(mask) else sparse_topk_mask
-
-        if exists(mask):
-            dots = dots.masked_fill(~mask, mask_value)
+        # causal
 
         if self.causal:
-            causal_mask = self.create_causal_mask(i, j, device = device)
-            dots = dots.masked_fill(causal_mask, mask_value)
+            i, j = sim.shape[-2:]
+            causal_mask = torch.ones((i, j), dtype = torch.bool, device = device).triu(j - i + 1)
+            sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
 
-        pre_softmax_attn = dots.clone()
+        # attention
 
-        attn = self.attn_fn(dots, dim = -1)
-        attn = attn.type(dtype)
-
-        post_softmax_attn = attn.clone()
-
+        attn = sim.softmax(dim = -1)
         attn = self.attn_dropout(attn)
 
-        if self.talking_heads:
-            attn = self.post_softmax_talking_heads(attn)
+        # aggregate values
 
-        out = einsum(f'b h i j, {kv_einsum_eq} -> b h i d', attn, v)
+        out = einsum(f"b h i j, b h j d -> b h i d", attn, v)
 
-        intermediates = Intermediates(
-            qk_similarities = qk_similarities,
-            pre_softmax_attn = pre_softmax_attn,
-            post_softmax_attn = post_softmax_attn
-        )
-
-        return out, intermediates
-
+        return out
